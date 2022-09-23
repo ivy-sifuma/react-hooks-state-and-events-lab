5d6979d1457ab27ad5eb4f0e358821d07fe1f7b6
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-09-23 23:06:45 +0300

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,21 @@ function App() {
   // this will be used for the Dark Mode Toggle feature
   const appClass = false ? "App dark" : "App light"
 
+  function handleClick (event) {
+    // console.log(event)
+     setIsDarkMode(!isDarkMode)
+   } 
+   
+    console.log(appClass)
+   
+
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
         <button>Dark Mode</button>
+
+        <button onClick = {handleClick}>{isDarkMode ? "Dark Mode" : "Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
